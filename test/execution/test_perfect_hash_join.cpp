a8ee576916d90e86f1b9b64e6a809497e641ad62
kmw14641 <102219400+kmw14641@users.noreply.github.com>
2025-08-04 10:37:24 +0000

    refactor: use helper function on perfect hash join test

--- test/execution/test_perfect_hash_join.cpp
+++ test/execution/test_perfect_hash_join.cpp
@@ -9,25 +9,31 @@
 
 using namespace duckdb;
 
-TEST_CASE("Test perfect hash join. Match one build input with one probe input") {
-    // 1. Set up input types: 2 integers
+void TestPerfectHashJoin(std::vector<std::vector<int>> left_data,
+                         std::vector<std::vector<int>> right_data,
+                         std::vector<std::vector<int>> expected_output) {
+    // Set up input types: 2 integers
     vector<LogicalType> left_input_types = {LogicalType::INTEGER, LogicalType::INTEGER};
     vector<LogicalType> right_input_types = {LogicalType::INTEGER, LogicalType::INTEGER};
 
-    // 2. Create input DataChunk
+    // Create input DataChunk
     DataChunk left_input;
     left_input.Initialize(left_input_types);
-    left_input.SetCardinality(1);
-    left_input.SetValue(0, 0, Value::INTEGER(10));  // left.col0 = 10
-    left_input.SetValue(1, 0, Value::INTEGER(20));  // left.col1 = 20
+    left_input.SetCardinality(left_data.size());
+    for (idx_t i = 0; i < left_data.size(); ++i) {
+        left_input.SetValue(0, i, Value::INTEGER(left_data[i][0]));  // left.col0
+        left_input.SetValue(1, i, Value::INTEGER(left_data[i][1]));  // left.col1
+    }
 
     DataChunk right_input;
     right_input.Initialize(right_input_types);
-    right_input.SetCardinality(1);
-    right_input.SetValue(0, 0, Value::INTEGER(20));  // right.col0 = 20
-    right_input.SetValue(1, 0, Value::INTEGER(30));  // right.col1 = 30
+    right_input.SetCardinality(right_data.size());
+    for (idx_t i = 0; i < right_data.size(); ++i) {
+        right_input.SetValue(0, i, Value::INTEGER(right_data[i][0]));  // right.col0
+        right_input.SetValue(1, i, Value::INTEGER(right_data[i][1]));  // right.col1
+    }
 
-    // 3. Create join expression: left.col1 = right.col0
+    // Create join expression: left.col1 = right.col0
     JoinCondition condition;
     condition.left = std::make_unique<BoundReferenceExpression>(LogicalType::INTEGER, 1);  // ref col 1
     condition.right = std::make_unique<BoundReferenceExpression>(LogicalType::INTEGER, 0);  // ref col 0
@@ -36,7 +42,7 @@ TEST_CASE("Test perfect hash join. Match one build input with one probe input") 
     vector<JoinCondition> conditions{};
     conditions.push_back(std::move(condition));
 
-    // 4. Create projections
+    // Create projections
     // left{col0, col1} + right{col0, col1} -> output{left.col0, left.col1, right.col0, right.col1}
     vector<uint32_t> left_projection_map = {0, 1};
     vector<uint32_t> right_projection_map = {2, 3};
@@ -44,16 +50,16 @@ TEST_CASE("Test perfect hash join. Match one build input with one probe input") 
     vector<LogicalType> right_build_types = {LogicalType::INTEGER, LogicalType::INTEGER};
     vector<idx_t> right_build_map{0, 1};
 
-    // 5. Set up schema
+    // Set up schema
     Schema schema;
     schema.setStoredTypes({LogicalType::INTEGER, LogicalType::INTEGER, LogicalType::INTEGER, LogicalType::INTEGER});  // output types
 
-    // 6. Create operator
+    // Create operator
     PhysicalPerfectHashJoin perfect_hash_join(schema, std::move(conditions), JoinType::INNER,
                                               left_projection_map, right_projection_map,
                                               right_build_types, right_build_map);
 
-    // 7. Execution context/state
+    // Execution context/state
     string dbdir = "/data/ldbc/sf1";
     auto db = make_unique<DuckDB>(dbdir.c_str());
     auto client_context = make_shared<ClientContext>(db->instance);
@@ -61,24 +67,33 @@ TEST_CASE("Test perfect hash join. Match one build input with one probe input") 
     auto sink_state = perfect_hash_join.GetLocalSinkState(exec_context);
     auto op_state = perfect_hash_join.GetOperatorState(exec_context);
 
-    // 8. Build Hash Table
+    // Build Hash Table
     auto sink_result = perfect_hash_join.Sink(exec_context, right_input, *sink_state);
 
-    // 9. Combine sink state (do nothing indeed)
+    // Combine sink state (do nothing indeed)
     perfect_hash_join.Combine(exec_context, *sink_state);
 
-    // 10. Create Output chunk
+    // Create Output chunk
     DataChunk output;
-    output.Initialize(schema.getStoredTypes(), 1);
+    output.Initialize(schema.getStoredTypes());
 
-    // 11. Probe Hash Table
+    // Probe Hash Table
     auto result = perfect_hash_join.Execute(exec_context, left_input, output, *op_state, *sink_state);
 
-    // 12. Check result
-    REQUIRE(result == OperatorResultType::NEED_MORE_INPUT);
-    REQUIRE(output.size() == 1);
-    REQUIRE(output.GetValue(0, 0) == Value::INTEGER(10));
-    REQUIRE(output.GetValue(1, 0) == Value::INTEGER(20));
-    REQUIRE(output.GetValue(2, 0) == Value::INTEGER(20));
-    REQUIRE(output.GetValue(3, 0) == Value::INTEGER(30));
+    // Check result
+    REQUIRE(result == OperatorResultType::HAVE_MORE_OUTPUT);
+    REQUIRE(output.size() == expected_output.size());
+    for (int i = 0; i < expected_output.size(); i++) {
+        for (size_t j = 0; j < expected_output[i].size(); j++) {
+            REQUIRE(output.GetValue(j, i) == expected_output[i][j]);
+        }
+    }
+}
+
+TEST_CASE("Test perfect hash join. Match one build input with one probe input") {
+    TestPerfectHashJoin(
+        {{10, 20}},  // left input
+        {{20, 30}},  // right input
+        {{10, 20, 20, 30}}  // expected output
+    );
 }
